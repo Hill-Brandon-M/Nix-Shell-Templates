18f6cd0afafc156a66f9e3aad9dc606dab91f3ea
= <=>
2025-04-10 19:43:09 -0700

    Add nix run derivation to empty flake template

--- empty/flake-only/flake.nix
+++ empty/flake-only/flake.nix
@@ -2,7 +2,7 @@
 
 {
   
-  description = "An empty template importing a nix shell";
+  description = "An empty flake template";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -11,6 +11,8 @@
   outputs = { self, nixpkgs, ... }: 
     let 
       
+      appName = "example";
+
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     
@@ -31,6 +33,29 @@
 
         # VARIABLE = "VALUE";
         # FILE = import ./something-else.nix
+      };
+
+      apps.${system}.default = pkgs.stdenv.mkDerivation {
+        
+        name = appName;
+        inherit system;
+
+        src = ".";
+
+        nativeBuildInputs = with pkgs; [
+          # Add any build dependencies here.
+        ];
+        
+        # TODO: Add build steps here.
+        buildPhase = ''
+          # ...
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/release/${appName} $out/bin/${appName}
+          chmod +x $out
+        '';
       };
     };
 }
