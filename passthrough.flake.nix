f9c331143378eb01f6dbcd4ee7c1767fb22ca6a9
Brandon Hill <brandon@mejino.family>
2025-04-10 18:42:55 -0700

    Make passthough flake template runnable via Nix

--- passthrough.flake.nix
+++ passthrough.flake.nix
@@ -10,10 +10,34 @@
 
   outputs = { self, nixpkgs, ... }: 
     let 
+      appName = "example";
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       devShells.${system}.default = (import ./shell.nix { inherit pkgs; });
+      
+      apps.${system}.default = pkgs.stdenv.mkDerivation {
+        
+        name = appName;
+        inherit system;
+
+        src = ".";
+
+        nativeBuildInputs = with pkgs; [
+          # Add any build dependencies here.
+        ];
+        
+        # TODO: Add build steps here.
+        buildPhase = ''
+          # ...
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/release/${appName} $out/bin/${appName}
+          chmod +x $out
+        '';
+      };
     };
 }
